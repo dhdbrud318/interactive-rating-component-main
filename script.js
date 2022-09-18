8c518609b76d2847fd163566450e1802b668d98a
yookeoungkate <82680611+dhdbrud318@users.noreply.github.com>
2022-09-17 17:02:12 -0700

    Change styles

--- script.js
+++ script.js
@@ -1,26 +1,21 @@
-const thankyou_page = document.querySelector("#thankyou-section");
-const rating_page = document.querySelector("#rating-section");
-const ratings = document.querySelectorAll("#rating");
-const score_section = document.querySelector("#user-score");
-const submit_btn = document.querySelector("#submit");
-let user_score;
+const rating_btns = document.querySelectorAll("input[type='radio']");
+const form = document.querySelector("#rating");
+const submit_btn = document.querySelector("button");
+const thankyou = document.querySelector("#thankyou");
+const displayScore = document.querySelector("#user-rating");
 
-function displayThankyou() {
-  thankyou_page.style.display = "block";
-  rating_page.style.display = "none";
-}
+let userScore;
 
-ratings.forEach((r) => {
-  r.addEventListener("click", function (e) {
-    user_score = e.target.value;
+form.addEventListener("click", function (e) {
+  submit_btn.disabled = false;
+  rating_btns.forEach((rating) => {
+    if (rating.checked) userScore = rating.value;
   });
 });
 
-submit.addEventListener("click", function () {
-  if (!user_score) {
-    window.alert("Please select a score!");
-    return;
-  }
-  score_section.innerText = user_score;
-  displayThankyou();
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  displayScore.innerText = userScore;
+  form.classList.toggle("disappear");
+  thankyou.classList.toggle("disappear");
 });
